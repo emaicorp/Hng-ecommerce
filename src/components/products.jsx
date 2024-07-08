24221c4e7a83c9a99ba417cfce4b2ff33bcf35bc
emaicorp <emaicorp@gmail.com>
2024-07-08 20:39:29 +0100

    moved images to public

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import productData from './items.json'; // Adjust the path to your JSON file
+import productData from './items'; // Adjust the path to your JSON file
 import AirFrier from '../assets/images/airfrier.png'
 
 const Product = () => {
@@ -9,9 +9,13 @@ const Product = () => {
       <h1>Product Listing</h1>
       <div className="row">
         {productData.products.map(product => (
-          <div key={product.id} className="col-md-4">
+            
+          <div key={product.id} className="col">
             <div className="card mb-4">
-              <img src={product.imgUrl} className="card-img-top" alt={product.name} />
+                <div className="card-img-top">
+                <img src={product.imgUrl} className="" alt={product.name} />
+                </div>
+              
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-text">${product.price}</p>
@@ -19,6 +23,7 @@ const Product = () => {
               </div>
             </div>
           </div>
+          
         ))}
       </div>
     </div>
