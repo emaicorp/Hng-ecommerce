af9600d733811de394f98146b6ffe2959f6c14e4
emaicorp <emaicorp@gmail.com>
2024-07-08 19:31:16 +0100

    testing images

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import productData from './items.json'; // Adjust the path to your JSON file
+import AirFrier from '../assets/images/airfrier.png'
 
 const Product = () => {
-    const importAll = (r) => {
-      let images = {};
-      r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
-      return images;
-    };
   
-    const images = importAll(require.context('../assets/images', false, /\.(png|jpe?g|svg)$/));
   return (
     <div className="container">
       <h1>Product Listing</h1>
@@ -16,7 +11,7 @@ const Product = () => {
         {productData.products.map(product => (
           <div key={product.id} className="col-md-4">
             <div className="card mb-4">
-              <img src={images[product.imgUrl]} className="card-img-top" alt={product.name} />
+              <img src={product.imgUrl} className="card-img-top" alt={product.name} />
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-text">${product.price}</p>
