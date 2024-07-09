0acbac47dc14aac98a1667f90e5c7240f31dcb59
emaicorp <emaicorp@gmail.com>
2024-07-09 22:55:49 +0100

    added toaster

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,22 +1,27 @@
+// src/contexts/CartContext.js
 import React, { createContext, useState } from 'react';
+import { toast } from 'react-toastify';
 
-// Create the context
 export const CartContext = createContext();
 
-// Create the provider component
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  const addToCart = (item) => {
-    setCart([...cart, item]);
-  };
-
-  const removeFromCart = (item) => {
-    setCart(cart.filter(cartItem => cartItem.id !== item.id));
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find(item => item.id === product.id);
+      if (existingProduct) {
+        toast.error('Item already in cart!');
+        return prevCart; // Item already exists, no changes needed
+      } else {
+        toast.success('Item added to cart!');
+        return [...prevCart, product]; // Add new item to cart
+      }
+    });
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
 
